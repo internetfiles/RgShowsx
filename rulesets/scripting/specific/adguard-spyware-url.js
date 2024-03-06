1001a03d554d5f7adc34f26c2b46378e07ec908f
Rishab Gautam <anitadevi04729@gmail.com>
2024-03-06 23:30:40 +0530

    few updates

---
+++ rulesets/scripting/specific/adguard-spyware-url.js
@@ -0,0 +1,1 @@
+"use strict";!function(){const a=new Map([["adguard.app",0],["adguard.com",0],["adguard.info",0]]),s=new Map(void 0),p=new Map(void 0);self.specificImports=self.specificImports||[],self.specificImports.push({argsList:[".hello_from_adguard_tracking_params"],hostnamesMap:a,entitiesMap:s,exceptionsMap:p})}();
