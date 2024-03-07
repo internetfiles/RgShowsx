782447baf37d0864974022ccd3622cc2d3f0a858
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-03-07 20:42:56 +0530

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,150 +1,259 @@
 // ==UserScript==
-// @name         mf-adblock
-// @namespace    none
-// @version      0.7
-// @description  A basic anti-adblock workaround that can remove or click elements on a website
-// @author       DerFichtl, with major improvements by Firepup650
-// @match        https://*/*
-// @icon         https://getadblock.com/favicon.ico
+// @name         Ads DOM Remover
+// @namespace    sagiegurari
+// @version      1.37
+// @author       Sagie Gur-Ari
+// @description  Removes Ad Containers from DOM (doesn't replace adblocker extension, but blocks dynamic content which the adblocker fails to block by removing whole sections from the HTML DOM.)
+// @homepage     https://github.com/sagiegurari/userscripts-ads-dom-remover
+// @supportURL   https://github.com/sagiegurari/userscripts-ads-dom-remover/issues
+// @match        https://www.ynet.co.il/*
+// @match        https://www.mynet.co.il/*
+// @match        https://www.calcalist.co.il/*
+// @match        https://www.globes.co.il/*
+// @match        https://sourceforge.net/projects/*/download*
+// @match        https://sourceforge.net/projects/*/postdownload*
+// @match        https://*.wikipedia.org/*
+// @match        https://*.techonthenet.com/*
+// @match        https://*.wikia.com/wiki/*
+// @match        https://*.reddit.com/*
+// @match        https://*.youtube.com/*
+// @match        https://*.geeksforgeeks.org/*
+// @match        https://*.bostonglobe.com/*
+// @match        https://*.aternos.org/*
+// @match        https://aternos.org/*
+// @require      https://code.jquery.com/jquery-2.2.2.min.js
+// @require      https://greasyfork.org/scripts/18490-ads-dom-remover-runner/code/Ads%20DOM%20Remover%20Runner.js?version=983896
 // @grant        none
-// @noframes
+// @license      MIT License
+// @downloadURL https://update.greasyfork.org/scripts/18491/Ads%20DOM%20Remover.user.js
+// @updateURL https://update.greasyfork.org/scripts/18491/Ads%20DOM%20Remover.meta.js
 // ==/UserScript==
 
-(function() {
+(function run($, runner) {
     'use strict';
-    (() => {
-        let oldPushState = history.pushState;
-        history.pushState = function pushState() {
-            let ret = oldPushState.apply(this, arguments);
-            window.dispatchEvent(new Event('pushstate'));
-            window.dispatchEvent(new Event('locationchange'));
-            return ret;
-        };
 
-        let oldReplaceState = history.replaceState;
-        history.replaceState = function replaceState() {
-            let ret = oldReplaceState.apply(this, arguments);
-            window.dispatchEvent(new Event('replacestate'));
-            window.dispatchEvent(new Event('locationchange'));
-            return ret;
-        };
-
-        window.addEventListener('popstate', () => {
-            window.dispatchEvent(new Event('locationchange'));
-        });
-    })();
-    let sites = {
-        /*
-        'example.com': { // website domain, can be regex.
-            remove: ['.some-class','#some-element'], // Hide these elements (used for ads) - (DOES NOT REMOVE THEM, JUST HIDES THEM)
-            click: ['.class','#element'], // Click these elements (used for cookie consent)
-            interaction: true, // Move mouse cursor to trigger `onmousemove` ads
-            timeout: 2000, // In MS, how long to wait before doing anything
-            interval: 0, // In MS, how long to wait before redoing everything (Ignored if 0 or missing) (used if ads are added onscroll or timeout)
-            onChange: true, // Redo everything once page changes, but no refresh/reload occurs (IE: Discourse)
-            antiVignette: true, // Auto fix google vignette based issues
-            trueRemove: true // Actually remove elements instead of hiding them (WARNING: Might trigger adblock detection!)
-            background: '#ffffff' // Set a background-color, overflow:scroll, and position (used for custom fullpage ads)
+    var selectorDefinitions = {
+        test: {
+            // the test selector definition is only used for testing
+            hostNames: [
+                '__tests1__',
+                '__tests2__'
+            ],
+            selectors: {
+                options: {
+                    loops: 1,
+                    interval: 1
+                },
+                selectors: [
+                    '.ad1',
+                    '.ad2'
+                ]
+            }
         },
-        */
-        '.*\.?fandom\.com': {
-            remove: ['.top-ads-container','.bottom-ads-container','#WikiaBar','.notifications-placeholder','.gpt-ad'],
-            interaction: true,
-            timeout: 2000,
-            trueRemove: true
-        },
-    }
-
-    let interval = null;
-
-    let hostname = document.location.hostname;
-
-    let loaded = false;
-
-    function cleanup() {
-        console.log("Running cleanup...");
-        if(sites[hostname].interaction) {
-            document.body.dispatchEvent(new MouseEvent('mousemove'));
-        }
-
-        if(sites[hostname].remove) {
-            let selectors = sites[hostname].remove;
-
-            selectors.forEach(function(selector) {
-                let elements = document.querySelectorAll(selector);
-
-                console.log(selector, elements);
-
-                elements.forEach(function(elem) {
-                    if (!sites[hostname].trueRemove) {
-                        elem.style.visibility = 'hidden';
-                        elem.style.width = '1px';
-                        elem.style.height = '1px';
-                        elem.style.overflow = 'hidden';
-                        elem.style.opacity = 0;
-                    } else {
-                        elem.remove();
+        ynet: {
+            hostNames: [
+                'ynet',
+                'calcalist'
+            ],
+            selectors: [
+                '#colorbox',
+                '#cboxOverlay',
+                '#ads.premium',
+                '#articleLayoutrightsidtable',
+                '#google_image_div',
+                'img[src*="dynamicyield"]',
+                'div.MSCmainContent',
+                '[id*="arketingCarouse"]',
+                '[id*="arketingRecommended"]',
+                '.mainVerticalArticleSharingLinks',
+                '.OUTBRAIN',
+                '.topBannerWrap',
+                '.block.B3 .B3.ghcite.dyother.dyMonitor div',
+                '.bigdealhomepage',
+                '#ww6s_Main',
+                '.buyandsavedy',
+                '.area.footer.ghcite',
+                '.hdr_set_homepage',
+                '#c1_Hor',
+                '#c2_Hor',
+                '#c3_Hor',
+                '#c4_Hor',
+                '#c5_Hor',
+                '#c6_Hor',
+                '.homepagevideo-x6',
+                '.buyandsave',
+                '.general-image',
+                '.PhotoArticlesTalkbacks',
+                '[name="ExternalWebpageIframe"]',
+                '#PROCOIL_SearchForm',
+                '#magazines1024',
+                '[id^="promo_"]',
+                '[id^="ads."]',
+                '[class*="facebook"]',
+                '[class*="WinWin"]',
+                '.main_search_radio',
+                'tr td [id^="ads."]',
+                '.art-action-wrp',
+                '.header-user-profile',
+                '.left-art-content',
+                '[class*="GeneralBanner"]',
+                '#vilon',
+                '#prime.shook',
+                '#articlebottomsharinglinks',
+                '.floatingPlayerimReallyDummy_container',
+                '#ynet_user_login',
+                '[title="YouTube"]',
+                '[title="facebook"]',
+                '#INDbtnWrap',
+                '.YnetPremiumHeaderLogin',
+                '.CreditLogos',
+                '.tp-modal',
+                {
+                    selector: '.homepagelitevideo',
+                    fineTuneSelector: function ($element) {
+                        return $element.parent().parent();
                     }
-                });
-            });
-        }
-
-        if(sites[hostname].background) {
-            document.body.style.background = sites[hostname].background;
-            document.body.style.overflow = 'scroll';
-            document.body.style.position = 'static';
-        }
-
-        if(sites[hostname].click) {
-            let selectors = sites[hostname].click;
-
-            selectors.forEach(function(selector) {
-                let element = document.querySelector(selector);
-
-                if(!!element) {
-                    element.click();
+                },
+                {
+                    selector: 'iframe',
+                    fineTuneSelector: function ($element) {
+                        return $element.filter(function () {
+                            return !$(this).parent().hasClass('news_ticker_iframe');
+                        });
+                    }
+                },
+                {
+                    selector: 'div.B2b.block div',
+                    pre: function ($element) {
+                        $element.parent().css({
+                            height: '1px'
+                        });
+                    }
                 }
-            });
+            ]
+        },
+        globes: {
+            hostNames: 'globes',
+            selectors: [
+                '#chromeWindow',
+                {
+                    selector: 'iframe',
+                    filter: function ($element) {
+                        return $element.not('#GlobalFinanceData_home[src~=/news/]');
+                    }
+                }
+            ]
+        },
+        techonthenet: {
+            hostNames: 'techonthenet.com',
+            selectors: ['.adsblocked']
+        },
+        sourceforge: {
+            hostNames: 'sourceforge.net',
+            selectors: [
+                '#content-for-adblock',
+                '#newsletter-floating',
+                '#page-body'
+            ]
+        },
+        wikipedia: {
+            hostNames: 'wikipedia.org',
+            selectors: [
+                '#frbanner',
+                '#frb-inline',
+                '#wlm-banner',
+                '#centralNotice',
+                '.frb-main',
+                '.frbanner',
+                '.frm',
+                '.frb'
+            ]
+        },
+        wikia: {
+            hostNames: 'wikia.com',
+            selectors: [
+                '.WikiaFooter',
+                '.WikiaRail',
+                '.wds-global-footer'
+            ]
+        },
+        reddit: {
+            hostNames: 'reddit.com',
+            selectors: [
+                '#onboarding-splash',
+                '[id^="google_ads"]'
+            ]
+        },
+        youtube: {
+            hostNames: 'youtube.com',
+            selectors: [
+                '.ytp-ce-element',
+                'ytd-companion-slot-renderer',
+                '#masthead-ad',
+                '.video-ads ytp-ad-module',
+                '.ytp-ad-overlay-ad-info-dialog-container',
+                '.ytp-ad-overlay-slot',
+                'tp-yt-paper-dialog'
+            ],
+            options: {
+                loops: 200,
+                interval: 2500
+            }
+        },
+        geeksforgeeks: {
+            hostNames: 'geeksforgeeks.org',
+            selectors: [
+                '.login-modal-div',
+                '[aria-label=cookieconsent]'
+            ]
+        },
+        bostonglobe: {
+            hostNames: 'bostonglobe.com',
+            selectors: [
+                '.meter-social-connect',
+                '.meter-social-connect__container'
+            ]
+        },
+        aternos: {
+            hostNames: 'aternos.org',
+            selectors: [
+                '.ad',
+                '.header-ad'
+            ]
         }
+    };
 
-        if(sites[hostname].antiVignette && window.location.hash && window.location.hash == "#google_vignette") {
-            window.location.href = window.location.href.split("#")[0]
-        }
-    }
+    [
+        '#dcPremiumRightImg',
+        '.boulevard',
+        '#multiarticles-9',
+        '#multiarticles-12',
+        '#multiarticles-13',
+        '#multiarticles-14',
+        '#multiarticles-15',
+        '#multiarticles-16',
+        '.CdaMostViews',
+        '.CdaCalcalistToday',
+        '.CdaRecomendedMovies',
+        '#SpecialBuilder1280',
+        '.cdaFooter1280'
+    ].forEach(function addSelector(selector) {
+        selectorDefinitions.ynet.selectors.push({
+            selector: selector,
+            fineTuneSelector: function ($element) {
+                return $element.parent();
+            }
+        });
+    });
 
-    if (!Object.keys(sites).indexOf(hostname) >= 0) {
-        for (const site in sites) {
-            const regex = new RegExp(site, "i");
-            if(regex.test(hostname)) {
-                hostname = site;
+    runner(
+        $, {
+            getSelectorDefinitions: function () {
+                return selectorDefinitions;
             }
         }
-    }
-
-    if(Object.keys(sites).indexOf(hostname) >= 0) {
-
-        let timeout = 0;
-        if(sites[hostname].timeout) {
-            timeout = sites[hostname].timeout;
-        }
-
-        window.setTimeout(function(){
-            cleanup();
-        }, timeout);
-
-        if(sites[hostname].interval && !interval) {
-            interval = window.setInterval(function(){
-                cleanup();
-            }, sites[hostname].interval);
-        }
-
-        if(sites[hostname].onChange && !loaded) {
-            loaded = true;
-            window.addEventListener('locationchange', function () {
-                window.setTimeout(function(){
-                    cleanup();
-                }, timeout);
-            });
-        }
-    }
-})();
+    );
+}(
+    window.jQuery.noConflict(true),
+    window.adrRunner
+));
