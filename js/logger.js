883ea64006126a70e779f7ad6a81d868cd99ec1c
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-14 16:20:29 +0530

    Update logger.js

--- js/logger.js
+++ js/logger.js
@@ -6,18 +6,9 @@ function sendToDiscord(ip, page, params) {
             title: 'New Access Logged',
             color: 0xff0000, // Red color
             fields: [
-                {
-                    name: 'IP Address',
-                    value: ip
-                },
-                {
-                    name: 'Page',
-                    value: page
-                },
-                {
-                    name: 'Parameters',
-                    value: params
-                }
+                { name: 'IP Address', value: ip },
+                { name: 'Page', value: page },
+                { name: 'Parameters', value: params }
             ]
         }]
     };
@@ -32,3 +23,21 @@ function sendToDiscord(ip, page, params) {
     .then(() => console.log('Data sent to Discord'))
     .catch(error => console.error('Error sending data to Discord:', error));
 }
+
+// Log IP address, page, and parameters on page load
+window.addEventListener('load', () => {
+    fetch('https://api64.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            const ip = data.ip;
+            const page = window.location.href;
+            const params = window.location.search;
+
+            console.log('IP Address:', ip);
+            console.log('Page:', page);
+            console.log('Parameters:', params);
+
+            sendToDiscord(ip, page, params);
+        })
+        .catch(error => console.error('Error getting IP address:', error));
+});
