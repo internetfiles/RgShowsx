22c9ed387344e048d0e3fe7d57668eafb66eb9fd
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-14 16:31:25 +0530

    Update nocheats.js

--- js/nocheats.js
+++ js/nocheats.js
@@ -1,31 +1,35 @@
 document.oncontextmenu = () => {
     alert("Pls dont try to steal the codes Restricted By Rishab")
     return false;
-
 }
 
 document.onkeydown = e => {
     if(e.key == "F12"){
         alert("Pls dont try to steal the codes Restricted By Rishab")
-        return false
+        return false;
     }
     if(e.ctrlKey && e.key == "u") {
         alert("Pls dont try to steal the codes Restricted By Rishab")
-        return false
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.key == "J") {
+        alert("Pls dont try to steal the codes Restricted By Rishab")
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.key == "C") {
+        alert("Pls dont try to steal the codes Restricted By Rishab")
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.key == "I") {
+        alert("Pls dont try to steal the codes Restricted By Rishab")
+        return false;
     }
 }
 
 document.addEventListener('keydown', function (e) {
-  if ((e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'i')) {
-    alert("Pls dont try to steal the codes Restricted By Rishab")
-    e.preventDefault();
-    return false
-  }
-    {
-  if ((e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.shiftKey && e.key === 'j')) {
-    alert("Pls dont try to steal the codes Restricted By Rishab")
-    e.preventDefault();
-    return false
-  }
+    if ((e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'i')) {
+        alert("Pls dont try to steal the codes Restricted By Rishab")
+        e.preventDefault();
+        return false;
+    }
 });
-    
