ce2a5acff1f9a968958015604fcd5f42a60a00db
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-14 16:35:04 +0530

    Update nocheats.js

--- js/nocheats.js
+++ js/nocheats.js
@@ -8,19 +8,19 @@ document.onkeydown = e => {
         alert("Pls dont try to steal the codes Restricted By Rishab")
         return false;
     }
-    if(e.ctrlKey && e.key == "u") || (e.ctrlKey && e.key == "U"){
+    if((e.ctrlKey && e.key == "u") || (e.ctrlKey && e.key == "U")){
         alert("Pls dont try to steal the codes Restricted By Rishab")
         return false;
     }
-    if(e.ctrlKey && e.shiftKey && e.key == "J") || (e.ctrlKey && e.key == "j") {
+    if((e.ctrlKey && e.shiftKey && e.key == "J") || (e.ctrlKey && e.key == "j")) {
         alert("Pls dont try to steal the codes Restricted By Rishab")
         return false;
     }
-    if(e.ctrlKey && e.shiftKey && e.key == "C") || (e.ctrlKey && e.key == "c") {
+    if((e.ctrlKey && e.shiftKey && e.key == "C") || (e.ctrlKey && e.key == "c")) {
         alert("Pls dont try to steal the codes Restricted By Rishab")
         return false;
     }
-    if(e.ctrlKey && e.shiftKey && e.key == "I") || (e.ctrlKey && e.key == "u") {
+    if((e.ctrlKey && e.shiftKey && e.key == "I") || (e.ctrlKey && e.key == "u")) {
         alert("Pls dont try to steal the codes Restricted By Rishab")
         return false;
     }
