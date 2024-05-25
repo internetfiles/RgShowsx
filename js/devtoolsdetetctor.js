4713e05ddd4bc5266ed48aa03523653294b1300a
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-25 23:36:29 +0530

    Update devtoolsdetetctor.js

--- js/devtoolsdetetctor.js
+++ js/devtoolsdetetctor.js
@@ -1,19 +1,20 @@
 (function() {
-    let devToolsOpened = false;
-    let lastBlockedTime = 0;
-    const blockTimeout = 5000; // 5 seconds
-
     // Function to detect if developer tools are open
     function detectDevTools() {
-        if (devToolsOpened || performance.now() > 100) {
-            devToolsOpened = true;
-            if (Date.now() - lastBlockedTime > blockTimeout) {
+        const element = new Image();
+        let devToolsOpened = false;
+
+        Object.defineProperty(element, 'id', {
+            get: function() {
+                devToolsOpened = true;
                 redirectToBlockedPage();
-                lastBlockedTime = Date.now();
             }
-        } else {
-            devToolsOpened = false; // Reset devToolsOpened if dev tools are not open
-        }
+        });
+
+        console.log(element);
+        setTimeout(() => {
+            devToolsOpened = false;
+        }, 1000);
     }
 
     // Function to redirect to blocked page
@@ -22,18 +23,22 @@
     }
 
     // Periodically check for dev tools
-    setInterval(detectDevTools, 1000); // Check every second
+    setInterval(detectDevTools, 1000);
+
+    // Check for dev tools by measuring execution time of a debugger statement
+    setInterval(() => {
+        const start = new Date();
+        debugger;
+        const end = new Date();
+        if (end - start > 100) {
+            redirectToBlockedPage();
+        }
+    }, 1000);
 
     // Check if the window is resized to an unusual size (like when dev tools are undocked)
     function detectWindowResize() {
-        if (window.outerWidth - window.innerWidth > 200 || window.outerHeight - window.innerHeight > 200) {
-            devToolsOpened = true;
-            if (Date.now() - lastBlockedTime > blockTimeout) {
-                redirectToBlockedPage();
-                lastBlockedTime = Date.now();
-            }
-        } else {
-            devToolsOpened = false; // Reset devToolsOpened if window resize is normal
+        if (window.outerWidth - window.innerWidth > 100 || window.outerHeight - window.innerHeight > 100) {
+            redirectToBlockedPage();
         }
     }
 
