d8854a46c21d7f401a0bb56e91b74b6a2947a4e3
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-25 21:29:08 +0530

    Update nocheats.js

--- js/nocheats.js
+++ js/nocheats.js
@@ -1,35 +1,28 @@
 function addNoCheatListeners(doc) {
     doc.oncontextmenu = () => {
-        alert("Pls dont try to steal the codes Restricted By Rishab");
         return false;
     };
 
     doc.onkeydown = e => {
         if (e.key === "F12") {
-            alert("Pls dont try to steal the codes Restricted By Rishab");
             return false;
         }
         if ((e.ctrlKey && e.key === "u") || (e.ctrlKey && e.key === "U")) {
-            alert("Pls dont try to steal the codes Restricted By Rishab");
             return false;
         }
         if ((e.ctrlKey && e.shiftKey && e.key === "J") || (e.ctrlKey && e.key === "j")) {
-            alert("Pls dont try to steal the codes Restricted By Rishab");
             return false;
         }
         if ((e.ctrlKey && e.shiftKey && e.key === "C") || (e.ctrlKey && e.key === "c")) {
-            alert("Pls dont try to steal the codes Restricted By Rishab");
             return false;
         }
         if ((e.ctrlKey && e.shiftKey && e.key === "I") || (e.ctrlKey && e.key === "i")) {
-            alert("Pls dont try to steal the codes Restricted By Rishab");
             return false;
         }
     };
 
     doc.addEventListener('keydown', function(e) {
         if ((e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'i')) {
-            alert("Pls dont try to steal the codes Restricted By Rishab");
             e.preventDefault();
             return false;
         }
