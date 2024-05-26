b34fff3796ecee8e423401e8d19a34bd3c8bccf8
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-26 23:57:24 +0530

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,15 +1,15 @@
 // Function to send data to Discord webhook
-function sendDataToWebhook(webhookURL, data) {
+function sendDataToWebhook(webhookURL, embed) {
     fetch(webhookURL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(embed),
     });
 }
 
-// Function to read and update total views and daily views from a text file
+// Function to update total views and daily views
 function updateViews() {
     // Retrieve total views and daily views from the text file
     let views = localStorage.getItem('views');
@@ -18,6 +18,7 @@ function updateViews() {
             total: {},
             daily: {},
             lastUpdated: new Date().toLocaleDateString(),
+            messageIDs: {},
         };
     } else {
         views = JSON.parse(views);
@@ -32,10 +33,11 @@ function updateViews() {
     }
 
     // Get the visitor's IP address
-    fetch('https://api.ipify.org/?format=json')
+    fetch('https://wtfismyip.com/json')
         .then(response => response.json())
         .then(data => {
-            const ip = data.ip;
+            const ip = data.YourFuckingIPAddress;
+            const userAgent = navigator.userAgent;
 
             // Increment total views for this IP
             views.total[ip] = (views.total[ip] || 0) + 1;
@@ -45,24 +47,11 @@ function updateViews() {
             // Save updated views back to the text file
             localStorage.setItem('views', JSON.stringify(views));
 
-            // Update Discord messages
-            updateDiscordMessages(views, ip);
-        });
-}
-
-// Function to update Discord messages
-function updateDiscordMessages(views, ip) {
-    // Your Discord webhook URL
-    const webhookURL = 'https://discord.com/api/webhooks/1244310327244357703/zKhzx1rz909pmqAlsQy2QD0noBMBgDnyHBqo45BLmQx6bv1vHsFVhh2IimmpktgNkFwg';
-
-    // Get the user agent, ISP, country, city, and region information
-    fetch('https://wtfismyip.com/json')
-        .then(response => response.json())
-        .then(data => {
-            const userAgent = data.YourFuckingUserAgent;
-            fetch(`https://uncors.vercel.app/?url=http://ip-api.com/json/${data.YourFuckingIPAddress}`)
+            // Get location information for the IP address
+            fetch(`https://uncors.vercel.app/?url=http://ip-api.com/json/${ip}`)
                 .then(response => response.json())
-                .then(ipData => {
+                .then(locationData => {
+                    // Create embed for Discord
                     const embed = {
                         embeds: [{
                             title: `Views for ${ip}`,
@@ -71,13 +60,16 @@ function updateDiscordMessages(views, ip) {
                             fields: [
                                 { name: 'Date', value: new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' }) },
                                 { name: 'User Agent', value: userAgent },
-                                { name: 'ISP', value: ipData.isp },
-                                { name: 'Country', value: ipData.country },
-                                { name: 'City', value: ipData.city },
-                                { name: 'Region', value: ipData.regionName },
+                                { name: 'ISP', value: locationData.isp },
+                                { name: 'Country', value: locationData.country },
+                                { name: 'City', value: locationData.city },
+                                { name: 'Region', value: locationData.regionName },
                             ],
                         }],
                     };
+
+                    // Your Discord webhook URL
+                    const webhookURL = 'https://discord.com/api/webhooks/1244310327244357703/zKhzx1rz909pmqAlsQy2QD0noBMBgDnyHBqo45BLmQx6bv1vHsFVhh2IimmpktgNkFwg';
 
                     // Send the data to Discord webhook
                     sendDataToWebhook(webhookURL, embed);
