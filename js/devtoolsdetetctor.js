50be38121c4f1ccb5c8f1e39a6e4bd012aa69e71
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-05-27 13:14:24 +0530

    Update devtoolsdetetctor.js

--- js/devtoolsdetetctor.js
+++ js/devtoolsdetetctor.js
@@ -2,24 +2,6 @@
     // Whitelisted IP addresses
     const whitelistedIPs = ['49.14.162.17', '2405:201:a006:9027:5104:a4b0:5ff9:c333'];
 
-    // Function to detect if developer tools are open
-    function detectDevTools() {
-        const element = new Image();
-        let devToolsOpened = false;
-
-        Object.defineProperty(element, 'id', {
-            get: function() {
-                devToolsOpened = true;
-                redirectToBlockedPage();
-            }
-        });
-
-        console.log(element);
-        setTimeout(() => {
-            devToolsOpened = false;
-        }, 1000);
-    }
-
     // Function to redirect to blocked page
     function redirectToBlockedPage() {
         window.location.href = '/blocked.html';
@@ -36,27 +18,15 @@
         .then(data => {
             const ip = data.ip;
             if (!isIPWhitelisted(ip)) {
-                // Periodically check for dev tools
-                setInterval(detectDevTools, 1000);
-
-                // Check for dev tools by measuring execution time of a debugger statement
-                setInterval(() => {
-                    const start = new Date();
-                    debugger;
-                    const end = new Date();
-                    if (end - start > 100) {
+                // Add listener to detect devtools
+                devtoolsDetector.addListener(isOpen => {
+                    if (isOpen) {
                         redirectToBlockedPage();
                     }
-                }, 1000);
+                });
 
-                // Check if the window is resized to an unusual size (like when dev tools are undocked)
-                function detectWindowResize() {
-                    if (window.outerWidth - window.innerWidth > 100 || window.outerHeight - window.innerHeight > 100) {
-                        redirectToBlockedPage();
-                    }
-                }
-
-                window.addEventListener('resize', detectWindowResize);
+                // Launch the detector
+                devtoolsDetector.launch();
             }
         })
         .catch(error => console.error('Error getting IP address:', error));
