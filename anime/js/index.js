c6d56b703e3c6844ae8d1eb5a4b38be1a2a8dfe2
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-06-27 00:40:41 +0530

    Update index.js

--- anime/js/index.js
+++ anime/js/index.js
@@ -64,7 +64,8 @@ function shuffle(array) {
     return array;
 }
 
-// Adding slider animes (trending animes from anilist)
+let slideIndex = 1;
+
 async function getTrendingAnimes(data) {
     let SLIDER_HTML = "";
 
@@ -82,13 +83,46 @@ async function getTrendingAnimes(data) {
             poster = anime["coverImage"]["extraLarge"];
         }
 
-        SLIDER_HTML += `<div class="mySlides fade"> <div class="data-slider"> <p class="spotlight">#${pos + 1
-            } Spotlight</p><h1>${title}</h1> <div class="extra1"> <span class="year"><i class="fa fa-play-circle"></i>${type}</span> <span class="year year2"><i class="fa fa-calendar"></i>${status}</span> <span class="cbox cbox1">${genres}</span> <span class="cbox cbox2">HD</span> </div><p class="small-synop">${description}</p><div id="watchh"> <a href="${url}" class="watch-btn"> <i class="fa fa-play-circle"></i> Watch Now </a> <a href="${url}" class="watch-btn watch-btn2"> <i class="fa fa-info-circle"></i> Details<i class="fa fa-angle-right"></i> </a> </div></div><div class="shado"> <a href="${url}"></a> </div><img src="${poster}"> </div>`;
+        SLIDER_HTML += `<div class="mySlides fade"> 
+            <div class="data-slider"> 
+                <p class="spotlight">#${pos + 1} Spotlight</p>
+                <h1>${title}</h1> 
+                <div class="extra1"> 
+                    <span class="year"><i class="fa fa-play-circle"></i>${type}</span> 
+                    <span class="year year2"><i class="fa fa-calendar"></i>${status}</span> 
+                    <span class="cbox cbox1">${genres}</span> 
+                    <span class="cbox cbox2">HD</span> 
+                </div>
+                <p class="small-synop">${description}</p>
+                <div id="watchh"> 
+                    <a href="${url}" class="watch-btn"> <i class="fa fa-play-circle"></i> Watch Now </a> 
+                    <a href="${url}" class="watch-btn watch-btn2"> <i class="fa fa-info-circle"></i> Details<i class="fa fa-angle-right"></i> </a> 
+                </div>
+            </div>
+            <div class="shado"> <a href="${url}"></a> </div>
+            <img src="${poster}"> 
+        </div>`;
     }
 
     document.querySelector(".slideshow-container").innerHTML =
         SLIDER_HTML +
         '<a class="prev" onclick="plusSlides(-1)">&#10094;</a><a class="next" onclick="plusSlides(1)">&#10095;</a>';
+
+    showSlides(slideIndex);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
 
 // Adding popular animes (popular animes from gogoanime)
